6eebc99800459c9b0884e71d71b82d8ab15da8f2
Salvatore Allegra <salallegra@gmail.com>
2025-03-02 18:25:42 -0800

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './styles.css'; // Importing the styles.css file
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,9 +9,7 @@ function App() {
       <div className='container'>
         <Header />
       </div>
-      <footer className="footer">
-        <p className='footer'>Footer</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
