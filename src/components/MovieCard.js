e3c33539e06fc6f34313673c42687c24d062d663
Salvatore Allegra <salallegra@gmail.com>
2025-03-10 13:30:51 -0700

    Add a dynamic css style for movie rating color

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,6 +5,16 @@ export default function MovieCard({ movie }) {
     event.target.src = "images/default.jpg";
   };
 
+  const getRatingClass = (rating) => {
+    if (rating >= 8) {
+      return "rating-good";
+    } else if (rating >= 4) {
+      return "rating-ok";
+    } else {
+      return "rating-bad";
+    }
+  };
+
   return (
     <div key={movie.id} className="movie-card">
       <img
@@ -14,7 +24,9 @@ export default function MovieCard({ movie }) {
       />
       <h3 className="movie-card-info">{movie.title}</h3>
       <p className="movie-genre">{movie.genre}</p>
-      <p className="movie-card-rating">{movie.rating}</p>
+      <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+        {movie.rating}
+      </p>
     </div>
   );
 }
