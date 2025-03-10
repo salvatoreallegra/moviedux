bbe7e80090de8ac983b33f2e69ef0f1139970508
Salvatore Allegra <salallegra@gmail.com>
2025-03-10 12:09:18 -0700

    Add error handler if there is a problem displaying the image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function MovieCard({ movie }) {
+  const handleError = (event) => {
+    event.target.src = "images/default.jpg";
+  };
+
   return (
     <div key={movie.id} className="movie-card">
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
       <h3 className="movie-card-info">{movie.title}</h3>
       <p className="movie-genre">{movie.genre}</p>
       <p className="movie-card-rating">{movie.rating}</p>
