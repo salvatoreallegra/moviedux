d7c5d12b13121e02306fb6c47ae1ece6026019ac
Salvatore Allegra <salallegra@gmail.com>
2025-03-10 14:50:23 -0700

    Change spacing @ sign in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "../styles.css";
 
-
 export default function Footer() {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-        <p className='footer'>© {currentYear} Movie Dux. All rights reserved.</p>
+      <p className="footer">©{currentYear} Movie Dux. All rights reserved.</p>
     </footer>
   );
 }
