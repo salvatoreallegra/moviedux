4be34b804f43ef9b588371ecfdc6c44f97af84f8
Salvatore Allegra <salallegra@gmail.com>
2025-03-10 09:33:48 -0700

    Add state hook

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from "react";
+import "../styles.css";
+
+
+export default function MoviesGrid() {
+
+const [movies, setMovies] = useState([]);
+
+  return (
+    <div className="">
+      
+    </div>
+  );
+}
